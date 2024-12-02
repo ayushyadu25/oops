34dd5bc5898460a29d162d02d0e01e6572011f07
ayushyadu25 <155137198+ayushyadu25@users.noreply.github.com>
2024-12-02 18:02:57 +0530

    Update experiment3a.cpp

--- Experiments/experiment3a.cpp
+++ Experiments/experiment3a.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <iostream>
 using namespace std;
 
@@ -53,60 +53,4 @@ int main()
     customer.displayBill();
 
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-class Electricity
-{
-    // private:
-    int units;
-    double billAmount;
-
-public:
-    Electricity() {} // default constructer
-
-    void setUnits(int u)
-    {
-        units = u;
-    }
-
-    void calculateBill()
-    {
-
-        if (units >= 0 && units < 100)
-        {
-            billAmount = units * 1.5;
-        }
-        else if (units < 200)
-        {
-            billAmount = 100 * 1.5 + (units - 100) * 2.0;
-        }
-        else
-        {
-            billAmount = (100 * 1.5) + (100 * 2) + (units - 200) * 3.0;
-        }
-    }
-
-    void displayBill()
-    {
-        cout << "Total bill amount: Rs " << billAmount << endl;
-    }
-};
-
-int main()
-{
-    Electricity customer;
-
-    int units;
-    cout << "Enter the number of units consumed: ";
-    cin >> units;
-
-    customer.setUnits(units);
-    customer.calculateBill();
-
-    customer.displayBill();
-
-    return 0;
->>>>>>> aeef39029bae6fa4762930a7cf212d2170919c01
 }
